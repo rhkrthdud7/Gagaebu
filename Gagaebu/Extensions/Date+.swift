9ee49effc835c608f75c632eff56b03ff1259f5e
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-05-21 22:31:29 +0900

    add Date extensions

--- Gagaebu/Extensions/Date+.swift
+++ Gagaebu/Extensions/Date+.swift
@@ -15,5 +15,28 @@ extension Date {
         formatter.dateFormat = "yyyy.MM.dd (E)"
         return formatter.string(from: self)
     }
+    
+    var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+
+    var endOfDay: Date {
+        var components = DateComponents()
+        components.day = 1
+        components.second = -1
+        return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfDay)!
+    }
+
+    var startOfMonth: Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: startOfDay)
+        return Calendar(identifier: .gregorian).date(from: components)!
+    }
+
+    var endOfMonth: Date {
+        var components = DateComponents()
+        components.month = 1
+        components.second = -1
+        return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfMonth)!
+    }
 
 }
